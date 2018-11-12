cb4124f9c5f186f05ae59c99cc9b7f517ed6951b
Matthew Macaspac <mcspc96@hotmail.com>
2018-11-11 22:40:08 -0700

    Add files via upload

--- AIStuff/EnemyMovement.cs
+++ AIStuff/EnemyMovement.cs
@@ -1,35 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour {
 	
 	Transform player;
 	PlayerHealth playerHealth;
 	EnemyHealth enemyHealth;
-	UnityEngine.AI.NavMeshAgent nav;
+	NavMeshAgent nav;
 	
-
+	public int minRange = 15;
+	
 	// Use this for initialization
-	void Awake () {
+	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player").transform;
 		playerHealth = player.GetComponent<PlayerHealth>();
 		enemyHealth = GetComponent <EnemyHealth>();
-		nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		nav = GetComponent<NavMeshAgent>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		//get the distance from enemey to the player
+		float distance = Vector3.Distance(transform.position, player.position);
+		
 		//If the enemey and the player have health
-		if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+		if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0 && distance < minRange)
 		{
-			//new destination of the player
-			nav.SetDestination(player.position);
+			//moves the enemy
+			Move();
 		}
 		else
 		{
 			nav.enabled = false;
 		}
+	}
+	
+	void Move() {
+		
+		//new destination of the player
+		nav.SetDestination(player.position);
 	}
 }
