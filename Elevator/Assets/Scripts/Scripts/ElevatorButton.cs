68b9b31df826cd7f02cc84b6fd589075d9d877e6
SaiSankaramanchi <spsankar@asu.edu>
2018-11-18 20:05:30 -0700

    Add files via upload

---
+++ Elevator/Assets/Scripts/Scripts/ElevatorButton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+public class ElevatorButton : MonoBehaviour 
+{
+	
+	
+	public GameObject m_ElevatorController;
+		
+	public bool ElevatorMoving;
+	
+	
+	public void SendCall (string Call) 
+	{
+		
+		ElevatorController Elevator = m_ElevatorController.GetComponent<ElevatorController>();
+		
+		ElevatorMoving = Elevator.ElevatorMoving;
+		
+		if(Call == "ElevatorUp" && !ElevatorMoving)
+		
+		{
+			
+			Elevator.ElevatorGO("ElevatorUp");
+		
+		}
+		
+		
+			if(Call == "ElevatorDown" && !ElevatorMoving)
+		
+			{
+			
+				Elevator.ElevatorGO("ElevatorDown");
+		
+			}
+	
+		}
+
+}
